6c3a3838f63f3c6a16ff957480fe67e10d8002a0
LordBedford <acater@gmail.com>
2021-02-03 19:56:45 -0500

    Syscalls lookin good

--- syscall.c
+++ syscall.c
@@ -14,16 +14,24 @@
 #include <stdlib.h>
 #include <sys/time.h>                                                                                
 #include <sys/syscall.h>
+#include <unistd.h>
 
 double avg_time = 0;
 
 void syscall_benchmark(){
-    int num_of_calls = 3000;
-    struct timeval current_time;
-    gettimeofday(&current_time, NULL);
-    printf("%ld", current_time.tv_usec);
-    syscall(0);
-    
+    int num_of_calls = 10000;
+    int i =0;
+    double total_time = 0;
+    for(;i<num_of_calls;i++)
+    {
+        struct timeval start;
+        struct timeval end;
+        gettimeofday(&start, NULL);
+        syscall(5);
+         gettimeofday(&end, NULL);
+        total_time = total_time + (end.tv_usec - start.tv_usec);
+    }
+    avg_time = total_time /num_of_calls;
 }
 
 int main(int argc, char *argv[]) {
@@ -32,8 +40,7 @@ int main(int argc, char *argv[]) {
 
     // Remember to place your final calculated average time
     // per system call into the avg_time variable
-
-    printf("Average time per system call is %f microseconds\n", avg_time);
     syscall_benchmark();
+    printf("Average time per system call is %f microseconds\n", avg_time);
     return 0;
 }
