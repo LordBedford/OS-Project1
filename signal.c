472dd1ef9c9a72c20761004329ce78e8e6aab79a
LordBedford <acater@gmail.com>
2021-02-07 17:17:17 -0500

    Cleaned up a bit I guess

--- signal.c
+++ signal.c
@@ -9,39 +9,9 @@
 void segment_fault_handler(int signum) {
 
     printf("I am slain!\n");
-
-    printf("%p\n", &signum);
-    //(signum) = (&signum) + 0xffffd5d0 + 0x8;
-    int* ptr = &signum;
-    /*
-    int i = 0;
-    for (i=0; i<=0xf; i++) {
-    	printf("Pointer int: %p\n", *(ptr+i));
-    }
-    */
-    ptr += 0xf;
-    //ptr = &ptr;
-    /*
-    while(ptr < 0xffffd6ac){
-        ptr += 0x1;
-    }
-    */
-
-    //0x5655557d
-    //printf("%p\n", ptr);
-    //printf("%x\n", *ptr);
-    
-    *ptr += 0x8;
-    
-    //int r2 = 0;
-    //r2 = *( (int *) 0 );
-    /*
-     * Bad instruction length is 8 bytes
-     */
-
-    /* Implement Code Here */
-
-
+        int* ptr = &signum;
+        ptr += 0xf;
+        *ptr += 0x8;
 }
 
 int main(int argc, char *argv[]) {
